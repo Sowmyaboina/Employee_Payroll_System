8a2c67219743f8973eef2007db59d620b1f7c749
Sowmyaboina <sowmyaboina01@gmail.com>
2024-03-26 23:04:00 +0530

    initial commit

--- src/com/project/payroll/app/UseEmployee.java
+++ src/com/project/payroll/app/UseEmployee.java
@@ -3,7 +3,7 @@ package com.project.payroll.app;
 public class UseEmployee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/com/project/payroll/beans/Employee.java
+++ src/com/project/payroll/beans/Employee.java
@@ -30,5 +30,44 @@ abstract class Employee {
 	public String toString() {
 		return "Employee[name = "+name+", id = "+id+", salary = "+calculateSalary()+"]";
 	}
+}
 
+
+//FullTime Employee class extends from Employee class
+class FullTimeEmployee extends Employee {
+	
+	//Monthly salary specific to Full Time Employee
+	private double monthlySalary;
+	
+	//Constructor of an Full Time Employee to initialize name , id , Salary
+	public FullTimeEmployee ( String name , int id , double monthlySalary  ) {
+		//Invoke the constructor of the superclass (Employee) to initialize name and id
+		super ( name, id );
+		//Assign the monthly salary specific to FullTimeEmployee
+		this.monthlySalary = monthlySalary;
+	}
+	
+	//overriding calculate salary from Employee class 
+	@Override
+	public double calculateSalary() {
+		return monthlySalary;
+	}
+}
+
+
+class PartTimeEmployee extends Employee {
+	
+	private int hoursWorked;
+	private double hourlyRate;
+	
+	public PartTimeEmployee ( String name, int id, int hoursWorked, double hourlyRate ) {
+		super ( name, id );
+		this.hoursWorked = hoursWorked;
+		this.hourlyRate = hourlyRate;
+	}
+	
+	@Override
+	public double calculateSalary() {
+		return hoursWorked * hourlyRate;
+	}
 }
